2c79f6afb6775da58e2e8951ce4c28c11643dbf8
hafisalrafi <hafisalrafi@office.itb.ac.id>
2019-02-24 12:04:15 +0700

    Add shake_reward layout

--- app/src/main/java/com/example/ok_food/RewardActivity.java
+++ app/src/main/java/com/example/ok_food/RewardActivity.java
@@ -3,6 +3,7 @@ package com.example.ok_food;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RewardActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class RewardActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rewards);
+    }
+
+    public void shakeNow(View view) {
+        Intent intent = new Intent(RewardActivity.this, )
     }
 }
