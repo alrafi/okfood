5b08acd7d4228c14f09fd23b4b941d3d3a42b12a
teresachn <33697410+teresachn@users.noreply.github.com>
2019-02-25 15:05:49 +0700

    Add firebase messaging service

--- app/src/main/java/com/example/ok_food/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/ok_food/MyFirebaseMessagingService.java
@@ -1,6 +1,42 @@
 package com.example.ok_food;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+    private static final String FIREBASE_NOTIFICATION_ID = "firebase_notification_channel";
+    private static final int NOTIFICATION_ID = 0;
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d(TAG, "Refreshed token: " + s);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+    }
+
+    public void showNotification(String title, String body) {
+        NotificationManager mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, FIREBASE_NOTIFICATION_ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.ic_notification);
+        mNotifyManager.notify(NOTIFICATION_ID, builder.build());
+    }
 }
